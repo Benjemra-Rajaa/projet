15f72fd9494732580fa6a15c61847d05b2805abb
Benjemra-Rajaa <rajae.asmae94@gmail.com>
2022-04-22 13:59:49 +0000

    test

--- app/Http/Controllers/FullCalenderController.php
+++ app/Http/Controllers/FullCalenderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\Event;
 
@@ -27,13 +28,37 @@ class FullCalenderController extends Controller
     	{
     		if($request->type == 'add')
     		{
-    			$event = Event::create([
+				$total_hours_db = 0;
+
+				$sql = "SELECT CAST(start as DATE) as date ,ROUND(SUM(time_to_sec(timediff(end,start))/3600),2)  as total_hours
+						FROM events 
+						WHERE title like '%$request->title%'
+						GROUP BY CAST(start as DATE)
+						HAVING date like '%". date('Y-m-d', strtotime($request->start)) ."%';";
+
+				$query = DB::select($sql);
+
+				if ($query) {
+					$total_hours_db = $query[0]->total_hours;
+				}
+
+				$diff_new_event = (strtotime($request->end) - strtotime($request->start)) /3600;
+
+				$total_hours = (int)$total_hours_db + $diff_new_event;
+
+				if ($total_hours > 8) {
+					return response()->json(['message' => 'Auncun rendez-vous est disponible  , veuillez choisir un autre jour'] , 400);
+				}else {
+					$event = Event::create([
     				'title'		=>	$request->title,
     				'start'		=>	$request->start,
     				'end'		=>	$request->end
     			]);
+				}
 
-    			return response()->json($event);
+    			
+
+    			return response()->json($event, 200);
     		}
 
     		if($request->type == 'update')
